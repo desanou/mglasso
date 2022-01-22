b56367459d4bcef7cfa838da9d294e72a6b343a2
Edmond Sanou <43121558+desanou@users.noreply.github.com>
2022-01-22 20:09:30 +0100

    update

--- R/utils.R
+++ R/utils.R
@@ -5,25 +5,12 @@
 #'
 #' @param i integer scalar. Indice of the first vector.
 #' @param j integer scalar. Indice of the second vector.
-<<<<<<< HEAD
-#' @param fun
-#' @param ni
-#' @param nj
-#' @param beta p by p numeric matrix. In rows, regression vectors coefficients after node-wise regression. `diag(beta) = 0`.
-#'
-#' @fun function. Applied on lines.
-#' @ni integer scalar. Weight for vector `i`.
-#' @nj integer scalar. Wweight for vector `j`.
-#'
-#' @return numeric scalar.
-=======
 #' @param fun function. Applied on lines.
 #' @param ni integer scalar. Weight for vector `i`.
 #' @param nj integer scalar. Wweight for vector `j`.
 #' @param beta p by p numeric matrix. In rows, regression vectors coefficients after node-wise regression. `diag(beta) = 0`.
 #'
 #' @return numeric vector
->>>>>>> initial
 #'
 #' @examples
 #'beta <- matrix(round(rnorm(9),2), ncol = 3)
@@ -62,10 +49,6 @@ cost <- function(beta, x, lambda1 = 0, lambda2 = 0) {
             norm(x[, i] - x %*% beta[i, ], type = "2")^2
         }))
 
-<<<<<<< HEAD
-        # I can vectorize it later
-=======
->>>>>>> initial
         for (i in 1:(p - 1)) {
             for (j in (i + 1):p) {
                 l2_norm <- l2_norm + norm(fun_lines(i, j, beta, `-`),
@@ -80,15 +63,10 @@ cost <- function(beta, x, lambda1 = 0, lambda2 = 0) {
     return(least_squares + lambda1 * l1_norm + lambda2 * l2_norm)
 }
 
-<<<<<<< HEAD
-#' symmetrize matrix of regression vectors pxp
-=======
-
 #' Apply symmetrization on estimated graph
 #'
 #' @param mat graph or precision matrix
 #' @param rule "and" or "or" rule
->>>>>>> initial
 symmetrize <- function(mat, rule = "and") {
     diag(mat) <- 0
     if (rule == "and") {
@@ -100,20 +78,9 @@ symmetrize <- function(mat, rule = "and") {
     return(mat)
 }
 
-<<<<<<< HEAD
-#' Title
-#'
-#' @param K
-#'
-#' @return
-#' @export
-#'
-#' @examples
-=======
 #' Compute precison matrix from regression vectors
 #'
 #' @param K precision matrix
->>>>>>> initial
 precision_to_regression <- function(K) {
     p <- ncol(K)
     mat <- matrix(0, p, p)
@@ -128,26 +95,10 @@ precision_to_regression <- function(K) {
     mat
 }
 
-<<<<<<< HEAD
-FUN_lines_wr <- function(i, j, Beta, FUN = `-`, ni = 1, nj = 1){
-  Y_coeffs <- Beta[i,]
-  X_coeffs <- Beta[j,]
-  Y_coeffs[j] <- 0
-  X_coeffs[i] <- 0
-  X_i <- X_coeffs[i]
-  X_coeffs[i] <- X_coeffs[j]
-  X_coeffs[j] <- X_i
-
-  FUN(ni*Y_coeffs, nj*X_coeffs)
-}
-
-#' vectorize beta matrix
-=======
 
 #' Transform a matrix of regression coefficients to vector removing the diagonal
 #'
 #' @param beta_mat matrix of regressions vectors
->>>>>>> initial
 beta_to_vector <- function(beta_mat){
   beta_mat <- as.matrix(beta_mat)
   diag(beta_mat) <- NA
@@ -156,36 +107,18 @@ beta_to_vector <- function(beta_mat){
   beta_mat
 }
 
-<<<<<<< HEAD
-#' Init Beta via OLS
-=======
-
 #' Initialize regression matrix
 #'
 #' @param X data
->>>>>>> initial
 beta_ols <- function(X){
   X <- as.matrix(X)
   p <- ncol(X)
   Beta <- matrix(0, nrow = p, ncol = p)
 
-<<<<<<< HEAD
-  # if(p <= nrow(X)){
-  #   f <- function(i){
-  #     bi <- as.vector(solve(t(X[,-i])%*%X[,-i]) %*% t(X[,-i]) %*% X[,i])
-  #     R.utils::insert(bi, ats = i, values = 0)
-  #   }
-  # }else{
-=======
->>>>>>> initial
   foo <- function(i){
     bi <- as.vector(corpcor::pseudoinverse(t(X[,-i])%*%X[,-i]) %*% t(X[,-i]) %*% X[,i])
     R.utils::insert(bi, ats = i, values = 0)
   }
-<<<<<<< HEAD
-  # }
-=======
->>>>>>> initial
 
   Beta <- sapply(1:p, foo)
   t(Beta)
