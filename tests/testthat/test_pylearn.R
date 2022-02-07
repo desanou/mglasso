8c20f7e3ed010fb9fa98cc38e009a1000abb0457
Edmond Sanou <43121558+desanou@users.noreply.github.com>
2022-02-07 11:15:03 +0100

    update test

--- tests/testthat/test_pylearn.R
+++ tests/testthat/test_pylearn.R
@@ -1,8 +1,6 @@
 testthat::test_that("pylearn-parsimony library is installed", {
   install_conesta()
-  skip_if_no_pylearn <- function() {
-    have_pylearn <- py_module_available("pylearn-parsimony")
-    if (!have_pylearn)
-      skip("scipy not available for testing")
-  }
+
+  testthat::expect_true(py_module_available("pylearn-parsimony"))
+
 })
