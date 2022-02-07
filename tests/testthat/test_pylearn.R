20ab1b0135908599637297fc47370ed287be6cb1
Edmond Sanou <43121558+desanou@users.noreply.github.com>
2022-02-07 12:37:09 +0100

    test pylearn

--- tests/testthat/test_pylearn.R
+++ tests/testthat/test_pylearn.R
@@ -1,6 +1,5 @@
 testthat::test_that("pylearn-parsimony library is installed", {
   install_conesta()
-
-  testthat::expect_true(reticulate::py_module_available("pylearn-parsimony"))
+  testthat::expect_true("pylearn-parsimony" %in% reticulate::py_list_packages()$package)
 
 })
