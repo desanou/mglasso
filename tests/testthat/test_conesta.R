5fa72bca9fb3023a30d0d5f3aec37cccb3c6bf05
Edmond Sanou <43121558+desanou@users.noreply.github.com>
2022-02-07 11:06:47 +0100

    comment test

--- tests/testthat/test_conesta.R
+++ tests/testthat/test_conesta.R
@@ -1,26 +1,26 @@
-testthat::test_that("conesta returns a matrix.", {
-
-  print("Testing conesta solver on a block diagonal model ...")
-
-  install_conesta()
-
-  n = 30
-  K = 2
-  p = 4
-  rho = 0.85
-  blocs <- list()
-  for (j in 1:K) {
-    bloc <- matrix(rho, nrow = p/K, ncol = p/K)
-    for(i in 1:(p/K)) { bloc[i,i] <- 1 }
-    blocs[[j]] <- bloc
-  }
-  mat.covariance <- Matrix::bdiag(blocs)
-
-  set.seed(11)
-  X <- mvtnorm::rmvnorm(n, mean = rep(0,p), sigma = as.matrix(mat.covariance))
-  X <- scale(X)
-
-  res <- conesta(X = X, lam1 = 0.1, lam2 = 0.1)
-
-  testthat::expect_true(is.matrix(res))
-})
+# testthat::test_that("conesta returns a matrix.", {
+#
+#   print("Testing conesta solver on a block diagonal model ...")
+#
+#   install_conesta()
+#
+#   n = 30
+#   K = 2
+#   p = 4
+#   rho = 0.85
+#   blocs <- list()
+#   for (j in 1:K) {
+#     bloc <- matrix(rho, nrow = p/K, ncol = p/K)
+#     for(i in 1:(p/K)) { bloc[i,i] <- 1 }
+#     blocs[[j]] <- bloc
+#   }
+#   mat.covariance <- Matrix::bdiag(blocs)
+#
+#   set.seed(11)
+#   X <- mvtnorm::rmvnorm(n, mean = rep(0,p), sigma = as.matrix(mat.covariance))
+#   X <- scale(X)
+#
+#   res <- conesta(X = X, lam1 = 0.1, lam2 = 0.1)
+#
+#   testthat::expect_true(is.matrix(res))
+# })
