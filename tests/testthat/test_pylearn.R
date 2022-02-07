a51386ddd825766ffe11dc93675be236bc4e5758
Edmond Sanou <43121558+desanou@users.noreply.github.com>
2022-02-07 11:19:57 +0100

    update test

--- tests/testthat/test_pylearn.R
+++ tests/testthat/test_pylearn.R
@@ -1,6 +1,6 @@
 testthat::test_that("pylearn-parsimony library is installed", {
   install_conesta()
 
-  testthat::expect_true(py_module_available("pylearn-parsimony"))
+  testthat::expect_true(reticulate::py_module_available("pylearn-parsimony"))
 
 })
