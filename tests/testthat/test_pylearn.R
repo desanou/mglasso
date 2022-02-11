db39a0014945085bddd8aa809576342aab6cc0a6
Edmond Sanou <43121558+desanou@users.noreply.github.com>
2022-02-11 18:00:34 +0100

    edit pylearn test

--- tests/testthat/test_pylearn.R
+++ tests/testthat/test_pylearn.R
@@ -1,5 +1,11 @@
 testthat::test_that("pylearn-parsimony library is installed", {
   install_conesta()
 
-  testthat::expect_true("pylearn-parsimony" %in% reticulate::py_list_packages()$package)
+  print("current environment")
+  print(reticulate::py_list_packages()$package)
+
+  print("Specified reticulate environment")
+  print(reticulate::py_list_packages("r-reticulate")$package)
+
+  #testthat::expect_true("pylearn-parsimony" %in% reticulate::py_list_packages()$package)
 })
