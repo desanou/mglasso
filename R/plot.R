8ea90fd6c94a2439f89467a5db9673656543bc63
Edmond Sanou <43121558+desanou@users.noreply.github.com>
2022-03-21 13:26:53 +0100

    add clusterpath function

--- R/plot.R
+++ R/plot.R
@@ -24,9 +24,58 @@ plot_mglasso <- function(mglasso_, levels_ = NULL) {
     image_sparse(mglasso_$out[[level]]$beta, "", level)
   })
 
-  l1 <- mglasso_$l1
+  l1 <- mglasso_$lambda1
 
   do.call(gridExtra::grid.arrange,
           c(pl, nrow = 2, top = paste0("lambda1 = ", l1)))
 
+}
+
+#' Plot MGLasso Clusterpath
+#'
+#' @param X numeric matrix
+#' @param mglasso_res object of class \code{mglasso}
+#'
+#' @details This function plot the clustering path of mglasso method on the 2 principal components
+#' axis of X. As the centroids matrices are not of the same dimension as X, we choose to plot the
+#' predicted X matrix path.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_clusterpath <- function(X, mglasso_res, colnames_ = NULL) {
+  ## Check inputs
+  stopifnot(class(mglasso_res) == "mglasso")
+
+  ## Initialisations
+  p <- ncol(X)
+  df.paths <- data.frame(x=c(),y=c(), group=c())
+  nlevel <- length(mglasso_res$out)
+
+  ## Principal component analysis
+  svdX <- svd(X)                ## singular value decomposition
+  pc <- svdX$u[,1:2,drop=FALSE] ## singular vectors
+
+  for (j in 1:nlevel) {
+    Beta <- mglasso_res$out[[j]]$beta
+    Xpred <- sapply(1:p, function(i){X %*% Beta[i,]})
+    pcs <- t(pc)%*%Xpred
+    x <- pcs[1,]
+    y <- pcs[2,]
+    df <- data.frame(x=pcs[1,], y=pcs[2,], group=1:p)
+    df.paths <- rbind(df.paths,df)
+  }
+
+  X_data <- as.data.frame(t(X)%*%pc) ## PCA projections (scores)
+  colnames(X_data) <- c("x","y")
+  ifelse(is.null(colnames_),
+         X_data$Name <- colnames(X),
+         X_data$Name <- colnames_)
+  data_plot <- ggplot(data=df.paths,aes(x=x,y=y))
+  data_plot <- data_plot + geom_path(aes(group=group),colour='grey30',alpha=0.5)
+  data_plot <- data_plot + geom_text_repel(data=X_data, aes(x=x,y=y,label=Name), max.overlaps = 20)
+  data_plot <- data_plot + geom_point(data=X_data,aes(x=x,y=y),size=1.5)
+  data_plot <- data_plot + xlab('Principal Component 1') + ylab('Principal Component 2')
+  data_plot + theme_bw()
 }
