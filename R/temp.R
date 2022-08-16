5090e70fef4329cb3e22bd374e79007e4fc21243
Edmond Sn <doedmond.sanou@univ-evry.fr>
2022-08-16 17:52:53 +0200

    comment

--- R/temp.R
+++ R/temp.R
@@ -10,25 +10,25 @@
 # reticulate::py_config()
 #
 
-install_conesta
-n = 50
-K = 3
-p = 9
-rho = 0.85
-blocs <- list()
-for (j in 1:K) {
-  bloc <- matrix(rho, nrow = p/K, ncol = p/K)
-  for(i in 1:(p/K)) { bloc[i,i] <- 1 }
-  blocs[[j]] <- bloc
-}
-
-mat.covariance <- Matrix::bdiag(blocs)
-mat.covariance
-
-set.seed(11)
-X <- mvtnorm::rmvnorm(n, mean = rep(0,p), sigma = as.matrix(mat.covariance))
-X <- scale(X)
-
-res <- mglasso(X, 0.1, lambda2_start = 0.1)
-res$out[[1]]$clusters
-res$out[[1]]$beta
+# install_conesta
+# n = 50
+# K = 3
+# p = 9
+# rho = 0.85
+# blocs <- list()
+# for (j in 1:K) {
+#   bloc <- matrix(rho, nrow = p/K, ncol = p/K)
+#   for(i in 1:(p/K)) { bloc[i,i] <- 1 }
+#   blocs[[j]] <- bloc
+# }
+#
+# mat.covariance <- Matrix::bdiag(blocs)
+# mat.covariance
+#
+# set.seed(11)
+# X <- mvtnorm::rmvnorm(n, mean = rep(0,p), sigma = as.matrix(mat.covariance))
+# X <- scale(X)
+#
+# res <- mglasso(X, 0.1, lambda2_start = 0.1)
+# res$out[[1]]$clusters
+# res$out[[1]]$beta
