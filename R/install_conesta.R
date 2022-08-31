fb63f69453674a0ff3d19c283e34d144c07dbb29
Edmond S <doedmondsanou@gmail.com>
2022-08-31 14:15:23 +0200

    use conda env instead of Python virtual envs

--- R/install_conesta.R
+++ R/install_conesta.R
@@ -7,19 +7,20 @@
 #' @export
 #'
 install_conesta <- function(extra_pack = c("scipy == 1.7.1", "scikit-learn", "numpy", "six",
-                                           "matplotlib"), py_version = '3.8.0') {
+                                           "matplotlib"), py_version = '3.8') {
 
-  is_rreticulate_env_installed = tryCatch(reticulate::use_virtualenv(envname = 'r-reticulate', required = TRUE),
+  is_rmglasso_env_installed = tryCatch(reticulate::use_condaenv(envname = 'rmglasso', required = TRUE),
                                           error = function (e) {'not installed'})
 
   # setup environment
-  if (!is.null(is_rreticulate_env_installed)) {
-    packageStartupMessage('mglasso requires the r-reticulate virtual environment. Attempting to create...')
-    #reticulate::virtualenv_create(envname = 'r-reticulate', python = py_version)
-    reticulate::virtualenv_create(envname = 'r-reticulate')
+  if (!is.null(is_rmglasso_env_installed)) {
+    packageStartupMessage('mglasso requires the rmglasso conda environment. Attempting to create...')
+    #reticulate::use_condaenv(envname = 'rmglasso', python = py_version)
+    reticulate::conda_create(envname = 'rmglasso', python_version = py_version)
+    message("Env created.")
   }
 
-  reticulate::use_virtualenv(virtualenv = 'r-reticulate', required = TRUE)
+  reticulate::use_condaenv(condaenv = 'rmglasso', required = TRUE)
 
   check_install <- sapply(extra_pack, reticulate::py_module_available)
 
@@ -27,24 +28,31 @@ install_conesta <- function(extra_pack = c("scipy == 1.7.1", "scikit-learn", "nu
 
   if(!identical(pack_to_install, character(0))){
     reticulate::py_install(pack_to_install,
-                           pip=TRUE,
-                           envname = "r-reticulate")
+                           envname = "rmglasso",
+                           python_version = py_version)
   }
 
   if (!reticulate::py_module_available("pylearn-parsimony")) {
-    reticulate::use_virtualenv(virtualenv = 'r-reticulate', required = TRUE)
-    message('Installing pylearn-parsimony')
+    # message('Installing pylearn-parsimony')
+    message('Configuring the env')
+    reticulate::use_condaenv(condaenv = 'rmglasso', required = TRUE)
+
 
     config <- reticulate::py_config()
+    message('Rmglasso env configured ')
     system2(config$python, c("-m", "pip", "install",
                              shQuote("git+https://github.com/neurospin/pylearn-parsimony.git")))
 
     message("pylearn-parsimony is installed.")
   }
+
+  the_module <- function()
+    try(reticulate::import_from_path("conesta_solver",
+                                     path = path_python()), silent = TRUE)
 }
 
 path_python <- function(){
   system.file("python", package = "mglasso")
 }
 
-the_module <- function() try(reticulate::import_from_path("conesta_solver", path = path_python()), silent = TRUE)
+
