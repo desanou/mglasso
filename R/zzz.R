1be68194081a78e20c2c5487a9c488c662104d53
Edmond S <doedmondsanou@gmail.com>
2022-08-31 14:15:57 +0200

    add onload function

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,7 @@
   packageStartupMessage("Welcome to MGLasso.")
   packageStartupMessage("Run install_conesta() to finalize the package python dependencies installation.")
 }
+
+.onLoad <- function(libname, pkgname) {
+  reticulate::configure_environment(pkgname, force = TRUE)
+}
