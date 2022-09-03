4db32bef77fd7b15600b61f9d1c012e6c45c68fc
Edmond S <doedmondsanou@gmail.com>
2022-09-03 22:12:55 +0200

    refactor: update/rename conesta function call

--- inst/python/conesta_solver.py
+++ inst/python/conesta_solver.py
@@ -4,7 +4,7 @@ import parsimony.algorithms as algorithms
 import sklearn.preprocessing 
 from scipy import sparse
 
-def conesta(X, lam1, lam2, beta_warm=None, type_="initial", W_=None, mean_ = False, max_iter_=1e4, prec_=1e-2):
+def conesta_py(X, lam1, lam2, beta_warm=None, type_="initial", W_=None, mean_ = False, max_iter_=1e4, prec_=1e-2):
   X=np.array(X)
   n=X.shape[0]
   p=X.shape[1]
