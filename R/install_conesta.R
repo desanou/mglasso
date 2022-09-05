15cdd81eafd2cbad7f56f29c75ecf301c67a1c7c
Edmond S <doedmondsanou@gmail.com>
2022-09-05 19:36:08 +0200

    feat: follow py_install structure for py packs

--- R/install_conesta.R
+++ R/install_conesta.R
@@ -6,37 +6,46 @@
 #'
 #' pylearn-parsimony contains the solver CONESTA used for the mglasso problem
 #' and is available on github at https://github.com/neurospin/pylearn-parsimony
-#' At this stage, we only support conda virtual environments.
-#'
-#'
-#' @param conda Character. Path to conda executable. "auto" finds the path automatically.
-#' @param extra_pack Character vector. Extra-packages to be installed.
-#' @param conda_py_version Character. Python version. It is advised to use a version ">=3.7,<3.10".
-#' The latest version under which mglasso was developped is scipy 1.7.1 which is based on python ">=3.7,<3.10".
+#' It is advised to use a python version ">=3.7,<3.10".
+#' Indeed, the latest version of scipy under which mglasso was developped is scipy 1.7.1 which is based on python ">=3.7,<3.10".
 #' In turn, this version of scipy can only be associated with a version of numpy ">=1.16.5,<1.23.0"
+#'
+#' @inheritParams reticulate::py_install
+#' @param extra_pack Character vector. Extra-packages to be installed.
+#' @param restart_session Restart R session after installing (note this will
+#'   only occur within RStudio)
+#' @param ... additionnal arguments passed to [`reticulate::py_install()`]
 #'
 #' @return No return value.
 #' @export
 #'
-install_pylearn_parsimony <- function(conda = "auto",
+install_pylearn_parsimony <- function(method = c("auto", "virtualenv", "conda"),
+                                      conda = "auto",
                                       extra_pack = c("scipy == 1.7.1",
                                                      "scikit-learn",
                                                      "numpy == 1.22.4",
                                                      "six",
                                                      "matplotlib"),
-                                      conda_py_version = '3.8',
+                                      python_version = '3.8',
                                       restart_session = TRUE,
-                                      envname = "rmglasso") {
+                                      envname = NULL,
+                                      ...) {
+
+
+  method <- match.arg(method)
 
   if (.Machine$sizeof.pointer != 8) {
     stop("Binary installation is only available for 64-bit platforms.")
   }
 
-  reticulate::py_install(packages = c(extra_pack, 'pylearn-parsimony'),
+  reticulate::py_install(packages = c("pylearn-parsimony", extra_pack),
                          envname = envname,
-                         python_version = conda_py_version,
+                         method = method,
+                         conda = conda,
+                         python_version = python_version,
                          pip = TRUE,
-                         pip_options = '-f https://github.com/neurospin/pylearn-parsimony/releases')
+                         pip_options = 'git+https://github.com/neurospin/pylearn-parsimony.git',
+                         ...)
 
   if (restart_session &&
       requireNamespace("rstudioapi", quietly = TRUE) &&
