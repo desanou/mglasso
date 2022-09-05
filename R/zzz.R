86b51b98962e321e181430cd6f4eb46be3bd8e05
Edmond S <doedmondsanou@gmail.com>
2022-09-05 19:33:09 +0200

    feat: delay load conesta solver

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,8 @@
 solver_module <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  reticulate::configure_environment(pkgname, force = TRUE)
-
-  solver_module <<- reticulate::import_from_path("conesta_solver",
-                                                 path = system.file("python", package = "mglasso"),
+   solver_module <<- reticulate::import_from_path("conesta_solver",
+                                                 path = system.file("python",
+                                                                    package = "mglasso"),
                                                  delay_load = TRUE)
 }
